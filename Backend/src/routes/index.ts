40441adf382505646083f5c91a10b58a85101a89
Cesar Goulart <devcesargoulart@gmail.com>
2020-07-29 18:04:14 -0300

    Aplicando Single Responsability Principle and Dependency Invertion Principle no Backend - Arquitetura SOLID

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,4 +1,12 @@
 import { Router } from 'express';
+import appointments from './appointments.routes';
 
 const routes = Router();
+
+routes.get('/', (req, res) =>
+  res.json({ message: 'Servidor rodando na porta 3333' }),
+);
+
+routes.use('/appointments', appointments);
+
 export default routes;
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-// import routes from './routes';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) =>
-  res.json({ message: 'Servidor rodando na porta 3333' }),
-);
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   global.console.log('  🚀 Server started on port 3333! 🚀');
